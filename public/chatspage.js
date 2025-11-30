1dda467c1d7abf47cb077d6a27e85df2603bae7a
Amani Kibet <amanikbt1@gmail.com>
2025-11-30 16:05:18 +0300

    c10

--- public/chatspage.js
+++ public/chatspage.js
@@ -170,12 +170,18 @@ const chatsPage = document.getElementById("CHATS-PAGE");
 const ICE_CONFIG = {
   iceServers: [
     {
-      urls: ["stun:stun.l.google.com:19302"],
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+        "stun:stun4.l.google.com:19302",
+      ],
     },
     {
-      urls: "turn:global.relay.metered.ca:80",
-      username: "openai",
-      credential: "openai",
+      urls: "turn:relay1.expressturn.com:3478",
+      username: "ef4KAvyJ3d2S0RIWAW",
+      credential: "e7w0oE00pq7AGa7r",
     },
   ],
 };
